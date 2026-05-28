34d2b9d9c585e7758814c6d05dbb9ef78c333020
StefanMarston <158752607+StefanMarston@users.noreply.github.com>
2026-05-28 17:15:35 +0200

    Update login mechanism in First.cpp
    Refactor login logic and variable types for username and password.

--- First.cpp
+++ First.cpp
@@ -2,10 +2,19 @@
 
 int main(){
   bool isLoggedIn = true;
-  std::string password = "123";
-  std::string username = "1234";
+  int username;
+  int password;
+  std::string user = "123";
+  std::string pass = "123!";
   
   while isLoggedIn == false {
-    std::cout << "log in";
+    std::cout << "log in\n";
+    std::cin >> username;
+    if (username == user) {
+      std::cout << "Enter your password:"; std::cin >> password;
+      if (password == pass) {
+        std::cout << "Logged in successfully!";
+        isLoggedIn = false;
+      }
     }
 }
