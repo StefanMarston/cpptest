fca346431199cf3bc1c0548fc59015a18cc34a73
StefanMarston <158752607+StefanMarston@users.noreply.github.com>
2026-05-28 17:18:42 +0200

    Update First.cpp

--- First.cpp
+++ First.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
+#include <string>
 
 int main(){
-  bool isLoggedIn = true;
-  int username;
-  int password;
+  bool isLoggedIn = false;
+  std::string username;
+  std::string password;
   std::string user = "123";
   std::string pass = "123!";
   
-  while isLoggedIn == false {
+  while (isLoggedIn == false) {
     std::cout << "log in\n";
     std::cin >> username;
     if (username == user) {
       std::cout << "Enter your password:"; std::cin >> password;
       if (password == pass) {
         std::cout << "Logged in successfully!";
-        isLoggedIn = false;
+        isLoggedIn = true;
       }
     }
+  }
 }
